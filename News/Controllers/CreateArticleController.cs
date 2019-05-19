7ee5418c556966e82e93a4858a7789c0ee797069
Vladislav Kipari <kippari.v@gmail.com>
2019-05-19 12:38:57 +0300

    Correct database and navbar with creating article on Journalist role

--- News/Controllers/CreateArticleController.cs
+++ News/Controllers/CreateArticleController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using News.Models;
 using NewsApp.Domain.Entities;
 using NewsApp.Persistence;
 using NewsApp.ReposInterfaces;
@@ -28,14 +30,16 @@ namespace News.Controllers
 
         public IActionResult CreateArticle()
         {
+            var model = new CategoryViewModel();
+            model.CategoryName = new SelectList(test.Categorys, "CategoryId", "Name", 1);
 
-            return View();
+            return View(model);
         }
      
 
 
         [HttpPost]
-        public async Task<IActionResult> UploadImage(IFormCollection form, string testTitle, string Text)
+        public async Task<IActionResult> UploadImage(IFormCollection form, string testTitle, string Text, CategoryViewModel model)
         {
             string storePath = "wwwroot/images/";
             if (form.Files == null || form.Files[0].Length == 0)
@@ -51,14 +55,14 @@ namespace News.Controllers
                 await form.Files[0].CopyToAsync(stream);
             }
 
-            StoreInDB(storePath + form.Files[0].FileName, testTitle, Text);
+            StoreInDB(storePath + form.Files[0].FileName, testTitle, Text, model);
 
             return RedirectToAction("CreateArticle");
 
         }
 
 
-        public void StoreInDB(string path, string testTitle, string Text)
+        public void StoreInDB(string path, string testTitle, string Text, CategoryViewModel model)
         {
 
             using (var con = new SqlConnection(connectionString))
@@ -77,14 +81,20 @@ namespace News.Controllers
 
 
             }
-            int lol = test.Articles.Count();
+            int articleId = test.Articles.Count();
             var article =
             from articl in test.Articles
             where articl.ArticleId == test.Articles.Count()
             select articl;
             var art = test.Articles.Where(w => w.ArticleId == test.Articles.Count()).FirstOrDefault();
-
-
+            var user = test.Users.Where(w => w.Email == User.Identity.Name).FirstOrDefault();
+            Category cat = test.Categorys.Find(model.SelectedCategoryId);
+            cat.Articles.Add(test.Articles.Find(articleId));
+            User journ = test.Users.Find(user.UserId);
+            art.Journalist = journ;
+            art.Journalist.UserId = journ.UserId;
+            art.Category = cat;
+            art.Category.CategoryId = cat.CategoryId;
             art.Title = testTitle;
             art.ArticleText = Text;
             test.SaveChanges();
---
+++ News/Models/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using NewsApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class CategoryViewModel
+    {
+
+        public int SelectedCategoryId { get; set; }
+        public SelectList CategoryName { get; set; }
+
+
+    }
+}
